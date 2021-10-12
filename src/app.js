7c84d8e60a584d8d8271a3f065e1635435513f4d
ldiffin <84605002+ldiffin@users.noreply.github.com>
2021-10-11 20:19:57 -0400

    Added weather description

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let humidityElement = document.querySelector("#humidity");
   let lowElement = document.querySelector("#temp-min");
+  let description = document.querySelector("#weather-description");
 
   cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   windElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = response.data.main.humidity;
   lowElement.innerHTML = Math.round(response.data.main.temp_min);
+  description.innerHTML = response.data.weather[0].description;
 
   celsiusTemperature = response.data.main.temp;
 }

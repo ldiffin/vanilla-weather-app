0649933c3ad87711514cb91847cc61aded495d1d
ldiffin <84605002+ldiffin@users.noreply.github.com>
2021-10-15 08:46:24 -0400

    Added API for Forecast

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
+function getForecast(coordinates) {
+  let apiKey = "0fa8548f277532461c6fd704d20ee563";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid={apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
@@ -20,9 +27,12 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
   celsiusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Tue", "Wed", "Thu"];
@@ -69,7 +79,6 @@ function displayFarhenheit(event) {
 }
 
 searchCity("Toronto");
-displayForecast();
 
 let celsiusTemperature = null;
 

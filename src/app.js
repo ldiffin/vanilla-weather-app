04c281db3f818f68736c40eb6a37dcf869a0e7a8
ldiffin <84605002+ldiffin@users.noreply.github.com>
2021-10-15 15:46:08 -0400

    Integrated API with forecast data

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 function getForecast(coordinates) {
   let apiKey = "0fa8548f277532461c6fd704d20ee563";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid={apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
@@ -31,23 +31,38 @@ function displayTemperature(response) {
   getForecast(response.data.coord);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Tue", "Wed", "Thu"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
       <div class="col future-forecast">
-        <span class="day">${day}</span>
+        <span class="day">${formatDay(forecastDay.dt)}</span>
         <br />
-        <i class="fas fa-sun"></i>
+        <img src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" alt="" width="42">
         <br />
-        <span>23°</span></div>`;
+        <span class="forecast-max">${Math.round(
+          forecastDay.temp.max
+        )}°/<span class="forecast-min">${Math.round(
+          forecastDay.temp.min
+        )}°</span></div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
